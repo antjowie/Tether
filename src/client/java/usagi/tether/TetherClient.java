a4b2b72a1c16f47f062e06885dadbd572071b1a7
Angelo Anthony Rettob <7332321+antjowie@users.noreply.github.com>
2023-06-03 21:34:04 +0200

    fix: updated to 1.20-rc and renamed template files

--- src/client/java/usagi/tether/TetherClient.java
+++ src/client/java/usagi/tether/TetherClient.java
@@ -2,7 +2,7 @@ package usagi.tether;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TetherClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
